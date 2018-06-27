3a268bbb74e85b84a8175e4c85dd0478cb2cdfdc
Patrick Rodgers <patrodg@microsoft.com>
2018-06-26 20:10:55 -0400

    Adding site inventory script, wip

---
+++ scripts/get-siteinventory/Get-WebInventory.ps1
@@ -0,0 +1,45 @@
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [String]$webUrl,
+    
+    [Parameter(Mandatory = $true)]
+    [SharePointPnP.PowerShell.Commands.Base.SPOnlineConnection]$connection,
+
+    [Parameter(Mandatory = $true)]
+    [String]$tempFolder
+)
+
+
+$web = Get-PnPWeb -Identity $webUrl -Connection $connection
+
+# get a listing of all the lists
+$listProperties = @(
+    "Title",
+    "Created",
+    "LastItemModifiedDate",
+    "LastItemUserModifiedDate",
+    "ItemCount",
+    "BaseType",
+    "DefaultViewUrl"
+)
+$listsCsvFile = Join-Path $tempFolder "web_$($web.Id)_lists.csv"
+$listWorkflowsCsvFile = Join-Path $tempFolder "web_$($web.Id)_workflows.csv"
+
+Get-PnPList -Web $web -Connection $connection -Includes "WorkflowAssociations" `
+    | ForEach-Object {
+
+    # report list details
+    $_ `
+        | Select-Object $listProperties `
+        | Add-Member @{WebUrl = $web.Url} -PassThru `
+        | Export-Csv -Path $listsCsvFile -Delimiter "," -NoTypeInformation -Append
+    
+    # report any workflow details
+    $_ `
+        | Select-Object "Title" -ExpandProperty "WorkflowAssociations" `
+        | Add-Member @{WebUrl = $web.Url} -PassThru `
+        | Export-Csv -Path $listWorkflowsCsvFile -Delimiter "," -NoTypeInformation -Append
+}
+
