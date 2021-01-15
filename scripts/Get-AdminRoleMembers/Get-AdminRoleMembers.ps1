2ef8badcb387fe2ec6ba7ec946f9aa7774f5c7c4
Brian Baldock <72090857+brianbaldock@users.noreply.github.com>
2021-01-14 21:28:14 -0500

    Update Get-AdminRoleMembers.ps1

--- scripts/Get-AdminRoleMembers/Get-AdminRoleMembers.ps1
+++ scripts/Get-AdminRoleMembers/Get-AdminRoleMembers.ps1
@@ -3,6 +3,7 @@ Write-Output "Please install the Azure AD Powershell Module to continue: https:/
 #>
 [CmdletBinding(DefaultParameterSetName='All')]
     param (
+        #Admin username
         [Parameter(Mandatory=$True,
         ParameterSetName='UPN',
         HelpMessage='Enter the admin account for the tenant - Example "admin@domain.com".')]
@@ -12,25 +13,40 @@ Write-Output "Please install the Azure AD Powershell Module to continue: https:/
         [Parameter(Mandatory=$True,
         ParameterSetName='All',
         HelpMessage='Enter the admin account for the tenant - Example "admin@domain.com".')]
-        [String]$Admin, 
+        [String]$Admin,
 
+        #Specify the export path for the CSV - Default is script directory
+        [Parameter(Mandatory=$False,
+        ParameterSetName='UPN',
+        HelpMessage='Enter the path for the CSV export (Default is the script directory)')]
+        [Parameter(Mandatory=$False,
+        ParameterSetName='RoleName',
+        HelpMessage='Enter the path for the CSV export (Default is the script directory)')]
+        [Parameter(Mandatory=$False,
+        ParameterSetName='All',
+        HelpMessage='Enter the path for the CSV export (Default is the script directory)')]
+        [String]$ExportPath,        
+
+        #Mandatory
         [Parameter(Mandatory=$true,
         ParameterSetName='UPN',
         HelpMessage='Enter the UserPrincipalName - Example "user@domain.com".')]
         [String]$UserPrincipalName,
 
+        #Mandatory
         [Parameter(Mandatory=$True,
         ParameterSetName='RoleName',
         HelpMessage='Enter the name of the Admin Role you would like to see members of.')]
         [String]$RoleName,
         
+        #Mandatory/Default
         [Parameter(Mandatory=$true,
         ParameterSetName='All',
         HelpMessage='This is the default parameter, will list all "active" admin roles and members.')]
         [switch]$All
     )
     
-    begin {
+    process {
         Try{
             Import-Module AzureAD
             try{
@@ -48,28 +64,53 @@ Write-Output "Please install the Azure AD Powershell Module to continue: https:/
                         $MemberList += $Table
                     } 
                 }
-                elseif($PSCmdlet.ParameterSetName -eq "UPN"){
-                    
+                if($PSCmdlet.ParameterSetName -eq "UPN"){
+                    $RoleList = Get-AzureADDirectoryRole
+                    foreach($Role in $RoleList){
+                        $RoleMembers = Get-AzureADDirectoryRoleMember -ObjectId $Role.ObjectID
+                        foreach($Member in $RoleMembers){
+                            if($Member.UserPrincipalName -eq $UserPrincipalName){
+                                $Table = New-Object PSObject -Property @{
+                                    ObjectID = ($Role.ObjectId)
+                                    RoleName = ($Role.DisplayName)
+                                    Member = ($Member.UserPrincipalName)
+                                }
+                            }
+                            $MemberList += $Table 
+                        }           
+                    }
                 }
-                elseif ($PSCmdlet.ParameterSetName -eq "RoleName"){
-                    
+                if ($PSCmdlet.ParameterSetName -eq "RoleName"){
+                    try{
+                        $VerifiedRoleName = Get-AzureADDirectoryRole | Where-Object -Property DisplayName -eq $RoleName
+                        $RoleMembers = Get-AzureADDirectoryRoleMember -ObjectId $VerifiedRoleName.ObjectId
+                        $Table = New-Object PSObject -Property @{
+                            ObjectID = ($VerifiedRoleName.ObjectID)
+                            RoleName = ($VerifiedRoleName.DisplayName)
+                            Member = ""
+                        }
+                        foreach($Member in $RoleMembers){
+                            $Table.Member += "$($Member.UserPrincipalName);"
+                        }
+                    }
+                    catch{
+                        return $_.Exception.Message
+                        break
+                    }
                 }
             }
             catch{
                 return $_.Exception.Message
+                break
             }
         }
-        Catch{
+        catch{
             Write-Output "Please install the Azure AD powershell module following the instructions at this link: https://aka.ms/AAau56t"
-            Break
+            break
         }
         
     }
-    
-    process {
         
-    }
-    
     end {
         return $MemberList
     }
