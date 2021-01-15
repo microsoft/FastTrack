a764aa012d8e52deac5862d897e350597546ea8b
Brian Baldock <72090857+brianbaldock@users.noreply.github.com>
2021-01-14 22:44:48 -0500

    Finalized for the most part

--- scripts/Get-AdminRoleMembers/Get-AdminRoleMembers.ps1
+++ scripts/Get-AdminRoleMembers/Get-AdminRoleMembers.ps1
@@ -1,9 +1,54 @@
 <#
-Write-Output "Please install the Azure AD Powershell Module to continue: https://docs.microsoft.com/en-us/powershell/azure/active-directory/install-adv2?view=azureadps-2.0"
+    .DESCRIPTION
+        This script is designed to display all of the members of the Azure AD Administrator Roles. You can filter by three
+        different parameters. Either "-All" displaying all members from all Administrator Roles, "-UserPrincipalName" to find
+        out which Roles a specific user is member of, or, "-RoleName" to display the members of a specific role group.
+         
+        The sample scripts are not supported under any Microsoft standard support 
+        program or service. The sample scripts are provided AS IS without warranty  
+        of any kind. Microsoft further disclaims all implied warranties including,  
+        without limitation, any implied warranties of merchantability or of fitness for 
+        a particular purpose. The entire risk arising out of the use or performance of  
+        the sample scripts and documentation remains with you. In no event shall 
+        Microsoft, its authors, or anyone else involved in the creation, production, or 
+        delivery of the scripts be liable for any damages whatsoever (including, 
+        without limitation, damages for loss of business profits, business interruption, 
+        loss of business information, or other pecuniary loss) arising out of the use 
+        of or inability to use the sample scripts or documentation, even if Microsoft 
+        has been advised of the possibility of such damages.
+
+        Author: Brian Baldock - brian.baldock@microsoft.com
+
+        Requirements: 
+            Have the Azure AD PowerShell module installed by following the instructions at this link: https://aka.ms/AAau56t"
+    
+    .PARAMETER Admin
+        Madatory Parameter - Admin account utilized for accessing the Microsoft 365 platform
+    
+    .PARAMETER All
+        Displays all members from all Administrator Roles. This is the default output
+
+    .PARAMETER UserPrincipalName
+        Specify a specific UserPrincipalName to display roles for that specific user
+
+    .PARAMETER RoleName
+        Specify a specifc roles name to get a list of users who are members of said role
+    
+    .EXAMPLE
+        Get a list of all the members of all the Administrator Roles in Azure AD
+        .\Get-AdminRoleMembers.ps1 -Admin admin@contoso.onmicrosoft.com -All
+
+    .EXAMPLE
+        Get a list of all the roles a user is member of
+        .\Get-AdminRoleMembers.ps1 -Admin admin@contoso.onmicrosoft.com -UserPrincipalName user@contoso.onmicrosoft.com
+
+    .EXAMPLE
+        Display the members for a particular Adminitrator Role
+        .\Get-AdminRoleMembers.ps1 -Admin admin@contoso.onmicrosoft.com -RoleName "Power BI Service Administrators"
 #>
+
 [CmdletBinding(DefaultParameterSetName='All')]
     param (
-        #Admin username
         [Parameter(Mandatory=$True,
         ParameterSetName='UPN',
         HelpMessage='Enter the admin account for the tenant - Example "admin@domain.com".')]
@@ -14,24 +59,21 @@ Write-Output "Please install the Azure AD Powershell Module to continue: https:/
         ParameterSetName='All',
         HelpMessage='Enter the admin account for the tenant - Example "admin@domain.com".')]
         [String]$Admin,
+        
+        [Parameter(Mandatory=$true,
+        ParameterSetName='All',
+        HelpMessage='This is the default parameter, will list all "active" admin roles and members.')]
+        [switch]$All,
 
-        #Mandatory
         [Parameter(Mandatory=$true,
         ParameterSetName='UPN',
         HelpMessage='Enter the UserPrincipalName - Example "user@domain.com".')]
         [String]$UserPrincipalName,
 
-        #Mandatory
         [Parameter(Mandatory=$True,
         ParameterSetName='RoleName',
         HelpMessage='Enter the name of the Admin Role you would like to see members of.')]
-        [String]$RoleName,
-        
-        #Mandatory/Default
-        [Parameter(Mandatory=$true,
-        ParameterSetName='All',
-        HelpMessage='This is the default parameter, will list all "active" admin roles and members.')]
-        [switch]$All
+        [String]$RoleName
     )
     
     begin {
@@ -64,7 +106,7 @@ Write-Output "Please install the Azure AD Powershell Module to continue: https:/
                     $Table = New-Object PSObject -Property @{
                         ObjectID = ($Role.ObjectId)
                         RoleName = ($Role.DisplayName)
-                        Member = (@($RoleMembers.UserPrincipalName) -join '; ')
+                        Member = (@($RoleMembers.UserPrincipalName) -join ', ')
                     }
                     $MemberList += $Table
                 }
@@ -113,5 +155,5 @@ Write-Output "Please install the Azure AD Powershell Module to continue: https:/
     }
 
     end {
-        return $MemberList
+        return $MemberList | Format-Table -Property ObjectID,RoleName,Member -AutoSize
     }
